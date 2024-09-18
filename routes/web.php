2c4145023ab7c0fd22d41c23da11636a139f3e4f
afyww <afyww18@gmail.com>
2024-09-18 23:27:19 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\PriceController;
 use App\Http\Controllers\PaymentController;
